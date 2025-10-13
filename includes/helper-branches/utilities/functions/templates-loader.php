e903eb44806d07e18bde92ade0ebabaa7605512c
Raul <rauldominguezsalgado@gmail.com>
2025-10-14 00:22:23 +0200

    Corrección puntual del loader de templates.

--- includes/helper-branches/utilities/functions/templates-loader.php
+++ includes/helper-branches/utilities/functions/templates-loader.php
@@ -9,23 +9,17 @@ if (!function_exists('iw_load_template')) {
      */
     function iw_load_template($template_name, $vars = array()) {
         // Rutas base
-        $user_template = get_plugin_dir('my-plugin-user') . '/templates/' . $template_name . '.php';
-        $core_template = get_plugin_dir('my-plugin') . '/templates/' . $template_name . '.php';
-
-        // Filtros opcionales (por si algún otro plugin quiere modificar las rutas)
-        $user_template = apply_filters('iw_user_template_path', $user_template, $template_name);
-        $core_template = apply_filters('iw_core_template_path', $core_template, $template_name);
+        $user_template = get_custom_helper_url() . 'includes/templates/' . $template_name . '.php';
+        $core_template = get_plugin_dir() . 'includes/templates/' . $template_name . '.php';
 
         // Determinar cuál existe
         $template_path = file_exists($user_template) ? $user_template : (file_exists($core_template) ? $core_template : false);
 
         if (!$template_path) {
-            do_action('iw_template_not_found', $template_name);
             trigger_error("Template no encontrado: {$template_name}", E_USER_WARNING);
             return;
         }
 
-        // Extraer las variables para el template
         if (!empty($vars) && is_array($vars)) {
             extract($vars, EXTR_SKIP);
         }
