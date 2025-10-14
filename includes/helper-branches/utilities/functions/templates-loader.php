bb637cf9546320f6117e24d30760bd1024569c72
Raul <rauldominguezsalgado@gmail.com>
2025-10-14 05:39:26 +0200

    Se ha modificado la funcion de templates para que busque en custom folder y tema

--- includes/helper-branches/utilities/functions/templates-loader.php
+++ includes/helper-branches/utilities/functions/templates-loader.php
@@ -9,11 +9,20 @@ if (!function_exists('iw_load_template')) {
      */
     function iw_load_template($template_name, $vars = array()) {
         // Rutas base
-        $user_template = get_custom_helper_url() . 'includes/templates/' . $template_name . '.php';
+        $theme_template = get_stylesheet_dir() . 'includes/templates/' . $template_name . '.php';
+        $user_template = get_custom_helper_dir() . 'includes/templates/' . $template_name . '.php';
         $core_template = get_plugin_dir() . 'includes/templates/' . $template_name . '.php';
 
         // Determinar cuál existe
-        $template_path = file_exists($user_template) ? $user_template : (file_exists($core_template) ? $core_template : false);
+        if (file_exists($theme_template)) {
+            $template_path = $theme_template;
+        } elseif (file_exists($user_template)) {
+            $template_path = $user_template;
+        } elseif (file_exists($core_template)) {
+            $template_path = $core_template;
+        } else {
+            $template_path = false;
+        }
 
         if (!$template_path) {
             trigger_error("Template no encontrado: {$template_name}", E_USER_WARNING);
