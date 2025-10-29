a2e55bf536233051176e00b14d0d9ba9e84ee1c8
Raul <rauldominguezsalgado@gmail.com>
2025-10-29 13:58:02 +0100

    Creacion y organización de la carpeta vendor para scripts de terceros.
    Se ha añadido la actualización desde GitHub.

--- indianwebs-helper.php
+++ indianwebs-helper.php
@@ -21,6 +21,19 @@ function iw_helper_load() {
     require_once __DIR__."/loader.php";
 }
 
+
+// Gestión de actualizaciones
+require 'vendor/plugin-update-checker-master';
+
+
+$updateChecker = YahnisElsts\PluginUpdateChecker\v5\PucFactory::buildUpdateChecker(
+    'https://github.com/RaulDominguezSalgado/indianwebs_helper',
+    __FILE__,
+    'iw-helper'
+);
+
+
+// Gestión al desinstalar
 register_uninstall_hook(__FILE__, 'iw_helper_uninstall');
 function iw_helper_uninstall() {
     Iw_Helper_DB_Manager::resetAll();
