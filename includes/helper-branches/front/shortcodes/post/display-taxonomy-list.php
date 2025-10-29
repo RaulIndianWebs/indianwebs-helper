b0c198afae9cad0a9039d7d1493a8faa281968c4
Raul <rauldominguezsalgado@gmail.com>
2025-10-29 12:06:07 +0100

    Se ha terminado de adaptar el shortcode taxonomy list a load template

--- includes/helper-branches/front/shortcodes/post/display-taxonomy-list.php
+++ includes/helper-branches/front/shortcodes/post/display-taxonomy-list.php
@@ -1,23 +1,33 @@
 <?php
 $atts = shortcode_atts(array(
-    "taxonomy-name" => "",
+    "taxonomy" => "",
     "depth"         => -1,
     "parent"        => 0,
     "layout"        => "default",
 ), $atts);
 
-if (empty($atts['taxonomy-name']) || !taxonomy_exists($atts['taxonomy-name'])) {
+if (empty($atts['taxonomy']) || !taxonomy_exists($atts['taxonomy'])) {
     return "<p>Taxonomía no válida.</p>";
+}
+
+$atts["terms"] = get_terms(array(
+    'taxonomy'   => $atts["taxonomy"],
+    'hide_empty' => false,
+    'parent'     => $atts["parent"],
+));
+
+if (empty($terms) || is_wp_error($terms)) {
+    return "<p>La taxonomia no tiene términos.</p>";
 }
 
 ob_start();
 
 iw_load_template('main/shortcodes/taxonomy-list', array(
-    'taxonomy' => $atts['taxonomy-name'],
+    'taxonomy' => $atts['taxonomy'],
     'part'    => "container-open",
 ), $atts["layout"]);
 
-iw_render_taxonomy_items($atts['taxonomy-name'], $atts["depth"], $atts["parent"], $atts["layout"]);
+iw_render_taxonomy_items($atts['terms'], $atts["depth"], $atts["layout"]);
 
 iw_load_template('main/shortcodes/taxonomy-list', array(
     'part'    => "container-close",
@@ -31,36 +41,31 @@ return ob_get_clean();
 /**
  * Carga recursiva de los términos usando templates
  */
-function iw_render_taxonomy_items($taxonomy, $depth, $parent,  $layout = "default", $current_depth = 0) {
-    if ($current_depth >= $depth) {
-        iw_load_template('main/shortcodes/taxonomy-list', array(
-            'term_name'          => $term->name,
-            'term_url'          => get_term_link($term),
-            'part'    => "child-single",
-        ), $layout);
-    }
-    else {
-        foreach ($terms as $term) {
-            $children = get_terms(array(
-                'taxonomy'   => $taxonomy,
-                'hide_empty' => false,
-                'parent'     => $term->term_id,
-            ));
+function iw_render_taxonomy_items($terms, $depth,  $layout = "default") {
+    foreach ($terms as $term) {
+        $children = get_terms(array(
+            'taxonomy'   => $term->taxonomy,
+            'hide_empty' => false,
+            'parent'     => $term->term_id,
+        ));
 
-            if (empty($children)) {
-                $single = true;
-            }
-            else {
-                iw_load_template('main/shortcodes/taxonomy-list', array(
-                    'term'          => $term,
-                    'part'    => "child-has-children-open",
-                ), $layout);
-                iw_render_taxonomy_items($taxonomy, $children, $current_depth + 1, $layout);
-                iw_load_template('main/shortcodes/taxonomy-list', array(
-                    'term'          => $term,
-                    'part'    => "child-has-children-close",
-                ), $layout);
-            }
+        if ($depth <= 1 || empty($children)) {
+            iw_load_template('main/shortcodes/taxonomy-list', array(
+                'term_name'          => $term->name,
+                'term_url'          => get_term_link($term),
+                'part'    => "child-single",
+            ), $layout);
+        }
+        else {
+            iw_load_template('main/shortcodes/taxonomy-list', array(
+                'term'          => $term,
+                'part'    => "child-has-children-open",
+            ), $layout);
+            iw_render_taxonomy_items($children, $depth - 1, $layout);
+            iw_load_template('main/shortcodes/taxonomy-list', array(
+                'term'          => $term,
+                'part'    => "child-has-children-close",
+            ), $layout);
         }
     }
 }
