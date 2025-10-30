ccbbe3a680bca552cf53f3c5b8e82fcf808a475b
Raul <rauldominguezsalgado@gmail.com>
2025-10-30 13:46:33 +0100

    Se ha creado la abse del bulk creation

--- includes/utilities/framework/classes/bulk-creation.php
+++ includes/utilities/framework/classes/bulk-creation.php
@@ -3,5 +3,13 @@
 namespace IW_Helper\Utilities\Framework\Admin;
 
 class IW_Helper_Bulk_Creation {
-    
+    private static array $base_dirs;
+
+    public static function add_dir($dir) {
+        self::$base_dirs[] = $dir;
+    }
+
+    public static function init() {
+        
+    }
 }
