a156e484c76e18c96ef357c875ea46ede8064c1d
Raúl Domínguez <rauld@indianwebs.com>
2026-05-19 13:35:52 +0200

    Modificación de los valores por defecto de developers tools

--- includes/config/developer-tools.php
+++ includes/config/developer-tools.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-    'wp-debug'        	=> 1,
-    'wp-debug-display'	=> 1,
-    'wp-debug-log'      => 1,
+    'wp-debug'        	=> 0,
+    'wp-debug-display'	=> 0,
+    'wp-debug-log'      => 0,
 );
