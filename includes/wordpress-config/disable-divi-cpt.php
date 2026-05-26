26e4b2522e5aa20d8ab59fdcbb2260a736be4f13
Raúl Domínguez <rauld@indianwebs.com>
2026-05-26 13:27:06 +0200

    Se ha modificado el hook de la desactivación del menu para que fucnione correctamente con WordPress 7

--- includes/wordpress-config/disable-divi-cpt.php
+++ includes/wordpress-config/disable-divi-cpt.php
@@ -2,12 +2,12 @@
 // Solo si la opción está activada
 if (!getPluginOptions("wordpress-config")["disable_divi_cpt"]) return;
 
-// 1) Ocultar del admin
-add_action('admin_init', function () {
-    remove_menu_page('edit.php?post_type=project'); // Quita el menu de Project
+
+add_action('admin_menu', function () {
+    remove_menu_page('edit.php?post_type=project');
 });
 
-// 2) Quitar soporte de Divi
+
 add_filter('et_builder_post_types', function($post_types){
     return array_diff($post_types, ['project']);
 });
@@ -15,13 +15,13 @@ add_filter('et_pb_blog_filterable_post_types', function($types){
     return array_diff($types, ['project']);
 });
 
-// 3) Quitar del REST API
+
 add_filter('rest_post_types', function($post_types){
     unset($post_types['project']);
     return $post_types;
 });
 
-// 4) Opcional: redirigir cualquier intento de acceder a un proyecto
+
 add_action('template_redirect', function () {
     if (is_singular('project') || is_post_type_archive('project')) {
         wp_redirect(home_url(), 301);
