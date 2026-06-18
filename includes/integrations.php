3fe30a97e9444f5d2230b4a08de0a8588df5262f
Raúl Domínguez <rauld@indianwebs.com>
2026-06-18 13:43:37 +0200

    Se han añadido nuevas integraciones

--- includes/integrations.php
+++ includes/integrations.php
@@ -1,11 +1,21 @@
 <?php
+// WooCommerce
 add_action( 'plugins_loaded', function () {
 	if ( class_exists( 'WooCommerce' ) ) {
         include_php_files(get_plugin_directory() . 'includes/integration/woocommerce/shortcodes');
         include_php_files(get_plugin_directory() . 'includes/integration/woocommerce/overides/');
     }
-    else if (function_exists( 'wpcf7' )) {
+    if (function_exists( 'wpcf7' )) {
     	include_php_files(get_plugin_directory() . 'includes/integration/cf7/');
+    }
+    if (defined("MTNC_VERSION")) {
+    	include_php_files(get_plugin_directory() . 'includes/integration/maintenance/');
+    }
+}, 11 );
+
+add_action( 'after_setup_theme', function () {
+    if (defined("ET_CORE_VERSION")) {
+        include_php_files(get_plugin_directory() . 'includes/integration/divi/');
     }
 }, 11 );
 
